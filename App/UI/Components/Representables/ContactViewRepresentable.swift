05bb75f253993db050d0b4edb808c6bbd67660d5
AnatoliPetrosyants <anatoli.petrosyants@sourcio2.com>
2023-09-12 11:18:30 +0400

    Add ContactViewRepresentable

---
+++ App/UI/Components/Representables/ContactViewRepresentable.swift
@@ -0,0 +1,25 @@
+//
+//  ContactViewRepresentable.swift
+//  Showcase
+//
+//  Created by Anatoli Petrosyants on 12.09.23.
+//
+
+import SwiftUI
+import ContactsUI
+
+struct ContactViewRepresentable: UIViewControllerRepresentable {
+
+    let contact: CNContact
+        
+    func makeUIViewController(context: Context) -> CNContactViewController {
+        let viewController = CNContactViewController(for: contact)
+        viewController.allowsEditing = true
+        viewController.allowsActions = true
+        return viewController
+    }
+    
+    func updateUIViewController(_ uiViewController: CNContactViewController, context: Context) {
+        // Update any properties or handle callbacks here if needed
+    }
+}
