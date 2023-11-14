00a9e05e9211882b4e6e483fe8564c67e503e8c7
Anatoli-Petrosyants <tolik.petrosyants@gmail.com>
2023-11-14 15:23:57 +0400

    feat: Modify app delegate

--- Shop/App/System/AppDelegate.swift
+++ Shop/App/System/AppDelegate.swift
@@ -11,21 +11,40 @@ import ComposableArchitecture
 import FirebaseCore
 import FirebaseFirestore
 
+/// The application delegate responsible for handling app-level events.
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    /// The shared instance of the QuickActionsHandler.
+    private let quickActionsHandler = QuickActionsHandler.shared
+    
+    /// The main store of the Composable Architecture.
     let store = Store(initialState: AppFeature.State()) {
         AppFeature()
     }
     
+    /// The method called when the application finishes launching.
+    ///
+    /// - Parameters:
+    ///   - application: The singleton app object.
+    ///   - launchOptions: A dictionary indicating the reason the app was launched (if any).
+    /// - Returns: `true` if the app was launched successfully; otherwise, `false`.
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-    ) -> Bool {        
+    ) -> Bool { 
+        // Configure Firebase
         FirebaseApp.configure()
+        
+        // Send an initial action to the store indicating that the app has finished launching
         self.store.send(.appDelegate(.didFinishLaunching))
         return true
     }
     
+    /// The method called when the app successfully registers for remote notifications.
+    ///
+    /// - Parameters:
+    ///   - application: The singleton app object.
+    ///   - deviceToken: A token that identifies the device to APNs.
     func application(
         _ application: UIApplication,
         didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
@@ -33,10 +52,41 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         self.store.send(.appDelegate(.didRegisterForRemoteNotifications(.success(deviceToken))))
     }
 
+    /// The method called when the app fails to register for remote notifications.
+    ///
+    /// - Parameters:
+    ///   - application: The singleton app object.
+    ///   - error: The error that occurred during registration.
     func application(
         _ application: UIApplication,
         didFailToRegisterForRemoteNotificationsWithError error: Error
     ) {
         self.store.send(.appDelegate(.didRegisterForRemoteNotifications(.failure(error))))
+    }
+    
+    /// The method called to provide a UISceneConfiguration.
+    ///
+    /// - Parameters:
+    ///   - application: The singleton app object.
+    ///   - connectingSceneSession: The new UISceneSession being created.
+    ///   - options: A dictionary of details about the launch environment.
+    /// - Returns: The newly created scene configuration.
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        // Check if the app was launched with a quick action
+        if let shortcutItem = options.shortcutItem {
+            quickActionsHandler.action = QuickAction(shortcutItem: shortcutItem)
+        }
+
+        // Create and configure the scene configuration
+        let configuration = UISceneConfiguration(
+            name: connectingSceneSession.configuration.name,
+            sessionRole: connectingSceneSession.role
+        )
+        configuration.delegateClass = SceneDelegate.self
+        return configuration
     }
 }
