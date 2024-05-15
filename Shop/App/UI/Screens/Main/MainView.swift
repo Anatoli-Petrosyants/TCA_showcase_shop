114a14ef997a60f5d35d1bc6c8b21a6429079104
Anatoli <tolik.petrosyants@gmail.com>
2024-05-15 12:10:15 +0400

    fix: Fix tab bar appearance

--- Shop/App/UI/Screens/Main/MainView.swift
+++ Shop/App/UI/Screens/Main/MainView.swift
@@ -84,12 +84,7 @@ extension MainView: View {
             .accentColor(.black)
             .onAppear {
                 let appearance = UITabBarAppearance()
-                appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
-                appearance.backgroundColor = UIColor(Color.gray.opacity(0.2))
-
-                // Use this appearance when scrolling behind the TabView:
-                UITabBar.appearance().standardAppearance = appearance
-                // Use this appearance when scrolled all the way up:
+                appearance.configureWithDefaultBackground()
                 UITabBar.appearance().scrollEdgeAppearance = appearance
             }
             
